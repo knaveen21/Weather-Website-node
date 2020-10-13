e1a3390b29ead840508aba5826d791af0aa142d3
Singh <768972@cognizant.com>
2020-10-13 15:22:57 +0530

    UI Change2

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.feelslike + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degree out. ' + 'The humidity is ' + body.current.humidity + '%')
         }
     })
 }
