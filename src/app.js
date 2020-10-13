1eb7982afd24de4974686a17fa4d486e8ecb23b0
Singh <768972@cognizant.com>
2020-10-13 11:22:52 +0530

    make it better

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define Path for  Express Config
 const publicDirectoryPath =path.join(__dirname, '../public')
@@ -117,8 +118,8 @@ app.get('*',(req, res) => {
     
 })
 
+//comments added
 
-
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server is up on port 3000')
 })
